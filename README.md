339b3e083b7a01ee25454ac52821ff4d28dadb2e
lorenzo.canovas <jlorenzocanovas@gmail.com>
2023-12-26 12:14:19 -0300

    feat: :tada: Init proyect

---
+++ README.md
@@ -0,0 +1,3 @@
+# Instagram Clone with React + Typescript + Tailwind CSS
+
+Comienzo del proyecto.
---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import './App.css';
+
+function App() {
+	return (
+		<>
+			<h1>Instagram Clone</h1>
+			<main>
+				<p>
+					Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora mollitia accusamus nesciunt labore et quia
+					iste dolor eum a quasi! Rem, ratione eius quaerat fugit quae id voluptatibus. Deleniti, quas?
+				</p>
+			</main>
+		</>
+	);
+}
+
+export default App;
