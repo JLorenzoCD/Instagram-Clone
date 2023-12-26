d5d1933b1d3d37f69e16a4e71cbc7cdb14915358
lorenzo.canovas <jlorenzocanovas@gmail.com>
2023-12-26 12:17:46 -0300

    feat: :heavy_plus_sign: Add tailwind CSS

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,7 @@
-import './App.css';
-
 function App() {
 	return (
 		<>
-			<h1>Instagram Clone</h1>
+			<h1 className='text-3xl font-bold underline'>Instagram Clone</h1>
 			<main>
 				<p>
 					Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora mollitia accusamus nesciunt labore et quia
