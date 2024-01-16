88e07cc85f23010d02e3a14499cde83e2e507d54
lorenzo.canovas <jlorenzocanovas@gmail.com>
2024-01-16 19:55:50 -0300

    fix: :adhesive_bandage: Correcting Stories component

--- src/components/Stories.tsx
+++ src/components/Stories.tsx
@@ -1,15 +1,18 @@
 import SkeletonProfile from './skeleton/Profile';
 
-import { myStorie, storiesData } from '../data/stories';
+import { userStorie, storiesData } from '../data/stories';
 
 function Stories() {
+	const userPostedAStory = false;
+
 	return (
 		<div className='max-w-full overflow-hidden md:max-w-2xl mx-auto p-3 mb-5'>
 			<ul className='flex space-x-6'>
-				<Storie {...myStorie} />
+				{userPostedAStory && <Storie {...userStorie} />}
+
 				{!!storiesData &&
 					storiesData.length !== 0 &&
-					storiesData.map((storie) => <Storie {...storie} key={storie.username} />)}
+					storiesData.map((storie) => <Storie {...storie} key={storie.id} />)}
 			</ul>
 		</div>
 	);
@@ -18,24 +21,18 @@ function Stories() {
 export default Stories;
 
 interface StorieProps {
+	id: number;
 	profile: string;
 	username: string;
-	isMe?: boolean;
 }
-export function Storie({ username, isMe = false }: StorieProps) {
+export function Storie({ username }: StorieProps) {
 	return (
 		<li className='flex flex-col items-center space-y-1'>
-			<div className={`${isMe ? 'relative' : ''} bg-gradient-to-tr from-yellow-400 to-purple-600 p-1 rounded-full`}>
+			<div className='bg-gradient-to-tr from-yellow-400 to-purple-600 p-1 rounded-full'>
 				<a href='#' className='block bg-white p-1 rounded-full transform transition hover:-rotate-6 size-12'>
 					{/* <img className='w-24 h-24 rounded-full' src={profile} alt={`instagram-profile-${username}`} /> */}
 					<SkeletonProfile className='size-full' />
 				</a>
-
-				{!!isMe && (
-					<button className='absolute bg-blue-500 text-white text-xl font-medium size-6 rounded-full -bottom-1 right-0 border-2 border-white font-mono hover:bg-blue-700 focus:outline-none leading-6'>
-						+
-					</button>
-				)}
 			</div>
 
 			<button className='text-xs'>{username}</button>
--- src/data/stories.ts
+++ src/data/stories.ts
@@ -1,36 +1,48 @@
-export const myStorie = {
+export const userStorie = {
+	id: 1,
 	profile: 'https://placekitten.com/200',
-	username: 'kitty_1',
-	isMe: true,
+	username: 'me',
 };
 
 export const storiesData = [
 	{
+		id: 10,
 		profile: 'https://placekitten.com/200',
 		username: 'kitty_1',
 	},
 	{
+		id: 11,
 		profile: 'https://placekitten.com/200',
 		username: 'kitty_2',
 	},
 	{
+		id: 12,
 		profile: 'https://placekitten.com/200',
 		username: 'kitty_3',
 	},
 	{
+		id: 13,
 		profile: 'https://placekitten.com/200',
 		username: 'kitty_4',
 	},
 	{
+		id: 14,
 		profile: 'https://placekitten.com/200',
 		username: 'kitty_5',
 	},
 	{
+		id: 15,
 		profile: 'https://placekitten.com/200',
 		username: 'kitty_6',
 	},
 	{
+		id: 16,
 		profile: 'https://placekitten.com/200',
 		username: 'kitty_7',
+	},
+	{
+		id: 17,
+		profile: 'https://placekitten.com/200',
+		username: 'kitty_8',
 	},
 ];
