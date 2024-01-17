6af0ec73a6ea27aaa4b2c9a0b93f46a06df85137
lorenzo.canovas <jlorenzocanovas@gmail.com>
2024-01-17 20:10:11 -0300

    Adding example data for stories

--- src/data/stories.ts
+++ src/data/stories.ts
@@ -8,41 +8,156 @@ export const storiesData = [
 	{
 		id: 10,
 		profile: 'https://placekitten.com/200',
-		username: 'kitty_1',
+		username: 'john',
 	},
 	{
 		id: 11,
 		profile: 'https://placekitten.com/200',
-		username: 'kitty_2',
+		username: 'emily',
 	},
 	{
 		id: 12,
 		profile: 'https://placekitten.com/200',
-		username: 'kitty_3',
+		username: 'michael',
 	},
 	{
 		id: 13,
 		profile: 'https://placekitten.com/200',
-		username: 'kitty_4',
+		username: 'susan',
 	},
 	{
 		id: 14,
 		profile: 'https://placekitten.com/200',
-		username: 'kitty_5',
+		username: 'david',
 	},
 	{
 		id: 15,
 		profile: 'https://placekitten.com/200',
-		username: 'kitty_6',
+		username: 'amelia',
 	},
 	{
 		id: 16,
 		profile: 'https://placekitten.com/200',
-		username: 'kitty_7',
+		username: 'brian',
 	},
 	{
 		id: 17,
 		profile: 'https://placekitten.com/200',
-		username: 'kitty_8',
+		username: 'olivia',
+	},
+	{
+		id: 18,
+		profile: 'https://placekitten.com/200',
+		username: 'chris',
+	},
+	{
+		id: 19,
+		profile: 'https://placekitten.com/200',
+		username: 'sophia',
+	},
+	{
+		id: 20,
+		profile: 'https://placekitten.com/200',
+		username: 'kevin',
+	},
+	{
+		id: 21,
+		profile: 'https://placekitten.com/200',
+		username: 'natalie',
+	},
+	{
+		id: 22,
+		profile: 'https://placekitten.com/200',
+		username: 'alex',
+	},
+	{
+		id: 23,
+		profile: 'https://placekitten.com/200',
+		username: 'emma',
+	},
+	{
+		id: 24,
+		profile: 'https://placekitten.com/200',
+		username: 'jason',
+	},
+	{
+		id: 25,
+		profile: 'https://placekitten.com/200',
+		username: 'mia',
+	},
+	{
+		id: 26,
+		profile: 'https://placekitten.com/200',
+		username: 'ryan',
+	},
+	{
+		id: 27,
+		profile: 'https://placekitten.com/200',
+		username: 'lily',
+	},
+	{
+		id: 28,
+		profile: 'https://placekitten.com/200',
+		username: 'owen',
+	},
+	{
+		id: 29,
+		profile: 'https://placekitten.com/200',
+		username: 'eva',
+	},
+	{
+		id: 30,
+		profile: 'https://placekitten.com/200',
+		username: 'jackson',
+	},
+	{
+		id: 31,
+		profile: 'https://placekitten.com/200',
+		username: 'hazel',
+	},
+	{
+		id: 32,
+		profile: 'https://placekitten.com/200',
+		username: 'nathan',
+	},
+	{
+		id: 33,
+		profile: 'https://placekitten.com/200',
+		username: 'scarlett',
+	},
+	{
+		id: 34,
+		profile: 'https://placekitten.com/200',
+		username: 'liam',
+	},
+	{
+		id: 35,
+		profile: 'https://placekitten.com/200',
+		username: 'zoey',
+	},
+	{
+		id: 36,
+		profile: 'https://placekitten.com/200',
+		username: 'ethan',
+	},
+	{
+		id: 37,
+		profile: 'https://placekitten.com/200',
+		username: 'ava',
+	},
+	{
+		id: 38,
+		profile: 'https://placekitten.com/200',
+		username: 'matthew',
+	},
+	{
+		id: 39,
+		profile: 'https://placekitten.com/200',
+		username: 'aubrey',
+	},
+	{
+		id: 40,
+		profile: 'https://placekitten.com/200',
+		username: 'caleb',
 	},
 ];
