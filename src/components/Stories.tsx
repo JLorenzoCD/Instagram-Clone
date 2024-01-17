23041561cb712d8250be435975e3c56bd61d6b27
lorenzo.canovas <jlorenzocanovas@gmail.com>
2024-01-17 20:11:39 -0300

    feat: :sparkles: Functional Stories component

--- src/components/Stories.tsx
+++ src/components/Stories.tsx
@@ -1,3 +1,4 @@
+import { useRef, useState } from 'react';
 import SkeletonProfile from './skeleton/Profile';
 
 import { userStorie, storiesData } from '../data/stories';
@@ -5,15 +6,65 @@ import { userStorie, storiesData } from '../data/stories';
 function Stories() {
 	const userPostedAStory = false;
 
+	const storiesRef = useRef<HTMLUListElement | null>(null);
+
+	const [showLeft, setShowLeft] = useState(false);
+	const [showRight, setShowRight] = useState(true);
+
+	const onScroll = () => {
+		if (storiesRef.current === null) return;
+
+		if (0 < storiesRef.current.scrollLeft) {
+			setShowLeft(true);
+		} else {
+			setShowLeft(false);
+		}
+
+		if (storiesRef.current.scrollLeft === storiesRef.current.scrollWidth - storiesRef.current.clientWidth) {
+			setShowRight(false);
+		} else {
+			setShowRight(true);
+		}
+	};
+
+	const scrollLeft = () => {
+		if (storiesRef.current == null) return;
+
+		storiesRef.current.scrollLeft = storiesRef.current.scrollLeft - storiesRef.current.clientWidth;
+	};
+
+	const scrollRight = () => {
+		if (storiesRef.current == null) return;
+
+		storiesRef.current.scrollLeft = storiesRef.current.scrollLeft + storiesRef.current.clientWidth;
+	};
+
 	return (
-		<div className='max-w-full overflow-hidden md:max-w-2xl mx-auto p-3 mb-5'>
-			<ul className='flex space-x-6'>
+		<div className='overflow-hidden max-w-xl mx-auto p-3 mb-5 relative'>
+			<ul className='flex space-x-2 overflow-x-scroll scroll-smooth no-scrollbar' onScroll={onScroll} ref={storiesRef}>
 				{userPostedAStory && <Storie {...userStorie} />}
 
 				{!!storiesData &&
 					storiesData.length !== 0 &&
 					storiesData.map((storie) => <Storie {...storie} key={storie.id} />)}
 			</ul>
+
+			{showLeft && (
+				<button
+					onClick={scrollLeft}
+					className='text-black align-top inline-block bg-white rounded-full size-6 cursor-pointer filter drop-shadow-lg absolute m-auto left-2 top-0 bottom-0 -translate-y-1/4'
+				>
+					〈
+				</button>
+			)}
+			{showRight && (
+				<button
+					onClick={scrollRight}
+					className='text-black align-top inline-block bg-white rounded-full size-6 cursor-pointer filter drop-shadow-lg absolute m-auto right-2 top-0 bottom-0 -translate-y-1/4'
+				>
+					〉
+				</button>
+			)}
 		</div>
 	);
 }
