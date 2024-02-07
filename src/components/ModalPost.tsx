99d4b848ee7e0159c8064c36b52cca386219bb1f
lorenzo.canovas <jlorenzocanovas@gmail.com>
2024-02-07 18:33:35 -0300

    refactor: :recycle: Separating example data

--- src/components/ModalPost.tsx
+++ src/components/ModalPost.tsx
@@ -12,26 +12,14 @@ import Comment from './Comment';
 
 import { getTimeAgo } from '../utilities/time';
 
+import { modalPostData } from '../data/modalPost';
+
 interface Props {
 	showModal: boolean;
 	closeModal: () => void;
 }
 
 export default function ModalPost({ showModal, closeModal }: Props) {
-	const modalPostData = {
-		user: {
-			name: 'username',
-			picture: '',
-			follow: false,
-		},
-		post: {
-			image: 'https://picsum.photos/275/385',
-			time: '2023-12-26T10:30:00Z',
-			description: '',
-			comments: [{ time: '2023-12-30T10:30:00Z', username: '', text: '' }],
-		},
-	};
-
 	return (
 		<Modal>
 			<div
---
+++ src/data/modalPost.ts
@@ -0,0 +1,13 @@
+export const modalPostData = {
+	user: {
+		name: 'username',
+		picture: '',
+		follow: false,
+	},
+	post: {
+		image: 'https://picsum.photos/275/385',
+		time: '2023-12-26T10:30:00Z',
+		description: '',
+		comments: [{ time: '2023-12-30T10:30:00Z', username: '', text: '' }],
+	},
+};
