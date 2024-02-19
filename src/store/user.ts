88fd641223555026140de168723079fc8a3b5522
lorenzo.canovas <jlorenzocanovas@gmail.com>
2024-02-18 22:51:39 -0300

    feat: :sparkles: Creating a store for the user with zustand

--- src/store/user.ts
+++ src/store/user.ts
@@ -6,7 +6,20 @@ import { currentUser } from '../data/user';
 
 interface IUserState {
 	currentUser?: ICurrentUser;
+	login: (currentUserData: ICurrentUser) => void;
+	logout: () => void;
 }
-export const useUserStore = create<IUserState>(() => ({
+export const useUserStore = create<IUserState>((set) => ({
+	// Initial State
 	currentUser: currentUser,
+
+	// Actions
+	login: (currentUserData: ICurrentUser) =>
+		set(() => ({
+			currentUser: currentUserData,
+		})),
+	logout: () =>
+		set(() => ({
+			currentUser: undefined,
+		})),
 }));
