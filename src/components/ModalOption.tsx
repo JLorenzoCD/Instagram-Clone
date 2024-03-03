9dde0708f6d69683091e594ff40eee6ac46b1fe3
lorenzo.canovas <jlorenzocanovas@gmail.com>
2024-03-03 20:29:54 -0300

    fix: :adhesive_bandage: Fix styles in ModalOption component

--- src/components/ModalOption.tsx
+++ src/components/ModalOption.tsx
@@ -37,7 +37,7 @@ export default function ModalOption({ show, closeModal }: Props) {
 							<ModalOptionItem disabled>Configuracion</ModalOptionItem>
 							<ModalOptionItem disabled>Tu actividad</ModalOptionItem>
 							<ModalOptionItem disabled>Guardado</ModalOptionItem>
-							<ModalOptionItem>
+							<ModalOptionItem disabled>
 								<CheckBoxInput>Cambiar apariencia ({themeLight ? 'light' : 'dark'})</CheckBoxInput>
 							</ModalOptionItem>
 							<ModalOptionItem disabled>Reportar un problema</ModalOptionItem>
@@ -69,7 +69,7 @@ function CheckBoxInput({ children }: { children: React.ReactNode }) {
 		<label className='inline-flex items-center cursor-pointer w-full justify-center mt-1'>
 			<input type='checkbox' value='' className='sr-only peer' />
 			<div className="relative w-11 h-6 bg-gray-200 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-blue-300 rounded-full peer  peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:start-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:w-5 after:h-5 after:transition-all peer-checked:bg-blue-600"></div>
-			<span className='ms-3 text-sm font-medium text-gray-900'>{children}</span>
+			<span className='ms-3 text-sm font-medium'>{children}</span>
 		</label>
 	);
 }
