bd5b706a29cb5a95703ea1b30c9379387caf41b5
lorenzo.canovas <jlorenzocanovas@gmail.com>
2024-03-08 19:01:19 -0300

    perf: :zap: Avoiding unnecessary data requests

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,4 +1,5 @@
 import { useUserStore } from '@/store/user';
+import { useScreenSize } from '@/hooks/useScreenSize';
 
 import Suggestions from '@/components/Suggestions';
 import SectionStories from '@/components/SectionStories';
@@ -9,6 +10,8 @@ function Home() {
 	const currentUser = useUserStore((state) => state.currentUser!);
 	const logout = useUserStore((state) => state.logout);
 
+	const { width } = useScreenSize(); // Avoiding unnecessary data requests
+
 	return (
 		<>
 			<div className='xl:grid xl:grid-cols-6'>
@@ -18,29 +21,31 @@ function Home() {
 					<SectionPostHome />
 				</div>
 
-				<aside className='hidden xl:block xl:col-span-2'>
-					<div className='fixed p-5 w-80'>
-						<div className='flex flex-row'>
-							<a href='#'>
-								<img className='rounded-full' src={currentUser.image} width='100' />
-							</a>
-							<div className='w-72 pl-2 m-auto'>
-								<div className='text-sm font-medium'>
-									<a href='#'>{currentUser.username}</a>
-								</div>
-								<div className='text-gray-500 text-sm leading-4'>{currentUser.name}</div>
-							</div>
-							<div className='w-32 text-right m-auto'>
-								<a className='text-xs text-sky-500 font-bold cursor-pointer' onClick={logout}>
-									Sign Out
+				{1280 <= width && (
+					<aside className='hidden xl:block xl:col-span-2'>
+						<div className='fixed p-5 w-80'>
+							<div className='flex flex-row'>
+								<a href='#'>
+									<img className='rounded-full' src={currentUser.image} width='100' />
 								</a>
+								<div className='w-72 pl-2 m-auto'>
+									<div className='text-sm font-medium'>
+										<a href='#'>{currentUser.username}</a>
+									</div>
+									<div className='text-gray-500 text-sm leading-4'>{currentUser.name}</div>
+								</div>
+								<div className='w-32 text-right m-auto'>
+									<a className='text-xs text-sky-500 font-bold cursor-pointer' onClick={logout}>
+										Sign Out
+									</a>
+								</div>
 							</div>
-						</div>
 
-						<Suggestions />
-						<Footer />
-					</div>
-				</aside>
+							<Suggestions />
+							<Footer />
+						</div>
+					</aside>
+				)}
 			</div>
 		</>
 	);
